86b7ca94fc4356aae12ef285ca0e874225758ecb
C0D1NGD0J0 <web-dev@United4Life.local>
2017-04-25 16:02:14 +0100

    added validations to user and product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
 	has_many :photos
+	validates :title, :description, :price,  presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :photo_ids, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  validates :username, presence: true, uniqueness: true, length: {minimum: 4, maximum: 15}
 end
